ad1f957eff1eda182d6767db70a59d7bdb2a43bd
centerfoward <jsu5438@naver.com>
2025-07-30 20:22:14 +0900

    [FEAT] Finish

---
+++ app/src/main/java/com/example/clone_instagram/data/model/FeedModel.kt
@@ -0,0 +1,10 @@
+package com.example.clone_instagram.data.model
+
+import org.w3c.dom.Text
+
+data class FeedModel(
+    val Uid : Text,
+    val profileImage : Text,
+    val feedImage : Text,
+    val feedDescription : Text
+)
